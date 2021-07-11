18497c7a09471667b10ebcf6adecec214ea191d3
jelbert55 <71341442+jelbert55@users.noreply.github.com>
2021-07-11 14:16:12 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Personal Website</title>
+  <link rel="stylesheet" href="css/generic.css">
+  <link rel="stylesheet" href="css/index.css">
+  <script src="js/index.js"></script>
+</head>
+  
+ <body>
+   hello my name is Justin
+  </body>
